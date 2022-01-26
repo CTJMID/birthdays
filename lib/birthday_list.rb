552e0ef611a54f2b97ebee53d9e57acea1322e5e
Chris Middleton <ctjmiddleton@gmail.com>
2022-01-26 12:57:51 +0000

    Tests two arguments and pushes to a hash

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,3 +1,5 @@
-def app_store(name, birthday)
-  "Birthday stored!"
+def app_store(name,birthday)
+  birthday_hash = {}
+  birthday_hash[name] = birthday
+  birthday_hash
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,7 +1,7 @@
 require 'birthday_list'
 
-describe 'app_store' do
-  it 'tells us a name and birthday have been stored correctly' do
-    expect(app_store("Chris", "June 5th")).to eq "Birthday stored!"
+describe '#app_store' do
+  it 'takes arguments and puts them into a hash as key value pairs' do
+    expect(app_store("Chris","June 5th")).to eq "Chris" => "June 5th"
   end
 end
